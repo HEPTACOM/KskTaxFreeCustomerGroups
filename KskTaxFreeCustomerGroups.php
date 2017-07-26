c36637c1b8309e5a94501fd4cea71e69a506c999
Julian Krzefski <krzefski@ksk-agentur.de>
2017-07-26 13:16:41 +0200

    Added event listener after sAdmin::sGetUserData to modify tax-free parameter

--- KskTaxFreeCustomerGroups.php
+++ KskTaxFreeCustomerGroups.php
@@ -3,11 +3,14 @@
 namespace KskTaxFreeCustomerGroups;
 
 use Doctrine\Common\Cache\CacheProvider;
+use Enlight_Event_EventArgs;
 use Shopware\Bundle\AttributeBundle\Service\CrudService;
 use Shopware\Bundle\AttributeBundle\Service\TypeMapping;
 use Shopware\Components\Model\ModelManager;
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Models\Attribute\CustomerGroup;
+use Shopware\Models\Customer\Customer;
 
 /**
  * Class KskTaxFreeCustomerGroups
@@ -43,5 +46,47 @@ class KskTaxFreeCustomerGroups extends Plugin
         $metaDataCache = $em->getConfiguration()->getMetadataCacheImpl();
         $metaDataCache->deleteAll();
         $em->generateAttributeModels(['s_core_customergroups_attributes']);
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Shopware_Modules_Admin_GetUserData_FilterResult' => 'filterUserData',
+        ];
+    }
+
+    /**
+     * @param Enlight_Event_EventArgs $args
+     */
+    public function filterUserData(Enlight_Event_EventArgs $args)
+    {
+        /** @var int $userId */
+        $userId = (int) $args->get('id');
+        /** @var array $userData */
+        $userData = $args->getReturn();
+
+        if ($userId === 0) {
+            return;
+        }
+
+        /** @var ModelManager $em */
+        $em = $this->container->get('models');
+
+        /** @var Customer $customer */
+        if (!($customer = $em->find(Customer::class, $userId)) instanceof Customer) {
+            return;
+        }
+
+        $group = $customer->getGroup();
+
+        if (!$group->getAttribute() instanceof CustomerGroup || $group->getAttribute()->getKskTaxFree() != true) {
+            return;
+        }
+
+        $userData['additional']['countryShipping']['taxfree'] = "1";
+        $args->setReturn($userData);
     }
 }
