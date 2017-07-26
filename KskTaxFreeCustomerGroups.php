9e6219ade3afa6acf621078d3aa95f3da2885879
Julian Krzefski <krzefski@ksk-agentur.de>
2017-07-26 13:12:12 +0200

    Initial commit

---
+++ KskTaxFreeCustomerGroups.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KskTaxFreeCustomerGroups;
+
+use Shopware\Components\Plugin;
+
+/**
+ * Class KskTaxFreeCustomerGroups
+ * @package KskTaxFreeCustomerGroups
+ */
+class KskTaxFreeCustomerGroups extends Plugin
+{
+}
